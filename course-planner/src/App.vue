6e7f86e1073babf3983e08fcf058d813aa3962e1
Casey Townsend <ctownsend@tecture.com>
2017-11-04 12:23:25 -0500

    Added intial design and mock data to app

--- course-planner/src/App.vue
+++ course-planner/src/App.vue
@@ -1,23 +1,126 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+<header>
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+		  <a class="navbar-brand" href="#"><img class="logo" src="./assets/logo.png"/></a>
+          <a class="navbar-brand" href="#">Your HCI Course Planner</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <!--<ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>-->
+		  <ul class="nav navbar-nav navbar-right">
+			  <li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown">Hi, Stuart&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-user"></span> <b class="caret"></b></a>
+				<ul class="dropdown-menu">
+				  <li><a href="#">Profile</a></li>
+				  <li><a href="#">Settings</a></li>
+				  <li><a href="#">Logout</a></li>
+				</ul>
+			  </li>
+		   </ul>
+      </div>
+        </div><!--/.nav-collapse -->
+    </nav>
+	</header>
+
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
+import HelloWorld from "./components/HelloWorld";
+
 export default {
-  name: 'app'
-}
+  name: "app",
+  components: {
+    HelloWorld
+  }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+/* header styles begin */
+.navbar {
+  background: #124684;
 }
+.navbar a,
+.navbar a:hover {
+  color: #ffffff;
+}
+.nav > li > a:focus,
+.nav > li > a:hover,
+.nav .open > a,
+.nav .open > a:focus,
+.nav .open > a:hover {
+  background: none;
+}
+.logo {
+  display: inline-block !important;
+  height: 40px;
+  width: 40px;
+  margin-top: -9px;
+}
+/* header styles end */
+
+/* left panel styles begin */
+.panel-group .panel,
+.panel {
+  border-radius: 0px;
+}
+.course_label {
+  padding: 5px 10px;
+  background: #124684;
+  color: #fff;
+  border-radius: 5px;
+  margin-bottom: 10px;
+}
+/* left panel styles end */
+
+/* course bucket styles begin */
+.bucket_wrapper {
+  overflow: auto;
+}
+.course_bucket {
+  background: #f5f5f5;
+  overflow: auto;
+  padding: 20px;
+  border: 1px solid #ddd;
+  margin-bottom: 20px;
+}
+.course_box {
+  background: #124684;
+  width: 175px;
+  margin-right: 20px;
+  display: inline-block;
+  padding: 15px;
+}
+.bucket_title {
+  color: #192f47;
+  font-size: 22px;
+  text-align: center;
+  margin-bottom: 20px;
+}
+.course_code {
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 22px;
+  text-align: center;
+}
+.course_name {
+  color: #ffffff;
+  text-align: center;
+  font-size: 16px;
+}
+/* course bucket styles end */
 </style>
--- course-planner/src/components/HelloWorld.vue
+++ course-planner/src/components/HelloWorld.vue
@@ -1,53 +1,195 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+<section>
+	<div class="container" id="coursePanel">
+		<div class="col-md-3">
+			<div class="panel-group" id="accordion">
+				 <div class="panel panel-default">
+					<div class="panel-heading">
+					  <h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+						  Introductory
+						</a>
+					  </h4>
+					</div>
+					<div id="collapseOne" class="panel-collapse collapse in">
+					  <div class="panel-body">
+							<div class="course_label">
+								<span>IT 411 - Scripting for Interactive Systems</span>
+							</div>
+							<div class="course_label">
+								<span>HCI 406 - Website Design for HCI</span>
+							</div>
+							<div class="course_label">
+								<span>HCI 402 - Foundations of Digital Design</span>
+							</div>
+							<div class="course_label">
+								<span>IT 403 - Statistics and Data Analysis</span>
+							</div>
+					  </div>
+					</div>
+				  </div>
+				 <div class="panel panel-default">
+					<div class="panel-heading">
+					  <h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+						  Foundation
+						</a>
+					  </h4>
+					</div>
+					<div id="collapseTwo" class="panel-collapse collapse">
+					  <div class="panel-body">
+							<div class="course_label">
+								<span>HCI 440 - Introduction to User-Centered Design</span>
+							</div>
+						  	<div class="course_label">
+								<span>HCI 441 - Introduction to User-Centered Development</span>
+							</div>
+							<div class="course_label">
+								<span>HCI 450 - Foundations of Human Computer Interaction</span>
+							</div>
+							<div class="course_label">
+								<span>HCI 430 - Prototyping and Implementation</span>
+							</div>
+					  </div>
+					</div>
+				  </div>
+				 <div class="panel panel-default">
+					<div class="panel-heading">
+					  <h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+						  Advanced
+						</a>
+					  </h4>
+					</div>
+					<div id="collapseThree" class="panel-collapse collapse">
+					  <div class="panel-body">
+							<div class="course_label">
+								<span>HCI 445 - Inquiry Methods and Use Analysis</span>
+							</div>
+						  	<div class="course_label">
+								<span>HCI 454 - Interaction Design and Information Architecture</span>
+							</div>
+							<div class="course_label">
+								<span>HCI 460 - Usability Evaluation Methods</span>
+							</div>
+							<div class="course_label">
+								<span>HCI 470 - Digital Design</span>
+							</div>
+					  </div>
+					</div>
+			</div>
+				 <div class="panel panel-default">
+					<div class="panel-heading">
+					  <h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+						  Major Electives
+						</a>
+					  </h4>
+					</div>
+					<div id="collapseFour" class="panel-collapse collapse">
+					  <div class="panel-body">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					  </div>
+					</div>
+				</div>
+				 <div class="panel panel-default">
+					<div class="panel-heading">
+					  <h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+						  Open Electives
+						</a>
+					  </h4>
+					</div>
+					<div id="collapseFive" class="panel-collapse collapse">
+					  <div class="panel-body">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-9">
+			<div class="bucket_wrapper">
+				<div class="course_bucket">
+					<div class="bucket_title">Introuductory Courses</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">IT 411</div>
+						<div class="course_name">Scripting for Interactive Systems</div>
+					</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">HCI 406</div>
+						<div class="course_name">Website Design for HCI</div>
+					</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">HCI 402</div>
+						<div class="course_name">Foundations of Digital Design</div>
+					</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">IT 403</div>
+						<div class="course_name">Statistics and Data Analysis</div>
+					</div>
+				</div>
+			</div>
+			<div class="bucket_wrapper">
+				<div class="course_bucket">
+					<div class="bucket_title">Foundation Courses</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">HCI 440</div>
+						<div class="course_name">Introduction to User-Centered Design</div>
+					</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">HCI 441</div>
+						<div class="course_name">Introduction to User-Centered Development</div>
+					</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">HCI 450</div>
+						<div class="course_name">Foundations of Human Computer Interaction</div>
+					</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">HCI 430</div>
+						<div class="course_name">Prototyping and Implementation</div>
+					</div>
+				</div>
+			</div>
+			<div class="bucket_wrapper">
+				<div class="course_bucket">
+					<div class="bucket_title">Advanced Courses</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">HCI 445</div>
+						<div class="course_name">Inquiry Methods and Use Analysis</div>
+					</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">HCI 454</div>
+						<div class="course_name">Interaction Design and Info. Arch.</div>
+					</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">HCI 460</div>
+						<div class="course_name">Usability Evaluation Methods</div>
+					</div>
+					<div class="course_box col-md-2">
+						<div class="course_code">HCI 470</div>
+						<div class="course_name">Digital Design</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div> 
+	</section>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      msg: "Welcome to Your Vue.js App"
+    };
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
