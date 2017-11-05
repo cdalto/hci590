32acf8abae67b1e999f337fa426c8a2cc69568f4
Casey Townsend <ctownsend@tecture.com>
2017-11-05 13:13:40 -0600

    Add single instance of course list for template

--- course-planner/src/components/CourseList.vue
+++ course-planner/src/components/CourseList.vue
@@ -1,26 +1,24 @@
 <template>
 	<div class="panel panel-default">
 		<div class="panel-heading">
-	  <h4 class="panel-title">
-		<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
-		  Introductory
-		</a>
-	  </h4>
+	  	<h4 class="panel-title">
+				<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Introductory</a>
+	  	</h4>
 		</div>
 		<div id="collapseOne" class="panel-collapse collapse in">
-	  <div class="panel-body">
-			<div class="course_label">
-				<span>IT 411 - Scripting for Interactive Systems</span>
-			</div>
-			<div class="course_label">
-				<span>HCI 406 - Website Design for HCI</span>
-			</div>
-			<div class="course_label">
-				<span>HCI 402 - Foundations of Digital Design</span>
-			</div>
-			<div class="course_label">
-				<span>IT 403 - Statistics and Data Analysis</span>
-			</div>
+	  	<div class="panel-body">
+				<div class="course_label">
+					<span>IT 411 - Scripting for Interactive Systems</span>
+				</div>
+				<div class="course_label">
+					<span>HCI 406 - Website Design for HCI</span>
+				</div>
+				<div class="course_label">
+					<span>HCI 402 - Foundations of Digital Design</span>
+				</div>
+				<div class="course_label">
+					<span>IT 403 - Statistics and Data Analysis</span>
+				</div>
 	  	</div>
 		</div>
 	</div>
@@ -31,7 +29,6 @@ export default {
   name: "CourseList",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
     };
   }
 };
