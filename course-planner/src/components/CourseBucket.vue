ea8a240b6ec947ce9412d21da79bfff877b1822b
Shivi <shivalika2612@gmail.com>
2017-11-11 13:06:44 -0600

    fixed html

--- course-planner/src/components/CourseBucket.vue
+++ course-planner/src/components/CourseBucket.vue
@@ -2,42 +2,35 @@
 	<div class="bucket_wrapper">
     <div class="course_bucket">
       <div class="bucket_title">{{heading}}</div>
-<<<<<<< HEAD
-		  <div v-for="course in list" class="course_box">
-		  <strong>{{course.code}}</strong> - {{course.name}}
-		  <span class="glyphicon glyphicon-info-sign pull-right" data-toggle="modal" data-target="#myModal"></span>
-	  	  </div>
-	  	  <div class="modal fade" id="myModal">
-	  		<div class="modal-dialog">
-				<div class="modal-content">
-					  <div class="modal-header">
-							<button type="button" class="close" data-dismiss="modal" aria-hidden="true" v-on:click.prevent="remove_course_details">&times;</button>
-							<h4 class="modal-title">Course Details</h4>
-					  </div>
-					  <div class="modal-body info_box" v-for="obj in CourseList">
-				  			<span>#{{obj.id}}</span><br/>
-					  		<strong>Course code: </strong><span>{{obj.code}}</span><br/>
-					  		<strong>Course name: </strong><span>{{obj.course}}</span><br/>
-					  		<strong>Course type: </strong><span>{{obj.type}}</span><br/>
-					  		<strong class="highlight">Offered in: </strong><span v-for="quarter in obj.offered"><span><br/>{{ quarter }}</span></span><br/>
-					  		<strong class="highlight">Prerequisites: </strong><span v-for="prerequisite in obj.prereq"><span></br/>{{ prerequisite }}</span></span>
-					  </div>
-					  <div class="modal-footer">
-							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-					  </div>
-				</div>
-	  		</div>
-		  </div> <!--Modal ends-->
-    </draggable>
+      	<draggable v-model="list" @start="drag=true" @end="drag=false" :options="{group: getPanelId}" class="bucket_area">
+			  <div v-for="course in list" class="course_box">
+				  <strong>{{course.code}}</strong> - {{course.name}}
+				  <span class="glyphicon glyphicon-info-sign pull-right" data-toggle="modal" data-target="#myModal"></span>
+		  	  </div>
+		  	  <div class="modal fade" id="myModal">
+		  		<div class="modal-dialog">
+					<div class="modal-content">
+						  <div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-hidden="true" v-on:click.prevent="remove_course_details">&times;</button>
+								<h4 class="modal-title">Course Details</h4>
+						  </div>
+						  <div class="modal-body info_box" v-for="obj in CourseList">
+					  			<span>#{{obj.id}}</span><br/>
+						  		<strong>Course code: </strong><span>{{obj.code}}</span><br/>
+						  		<strong>Course name: </strong><span>{{obj.course}}</span><br/>
+						  		<strong>Course type: </strong><span>{{obj.type}}</span><br/>
+						  		<strong class="highlight">Offered in: </strong><span v-for="quarter in obj.offered"><span><br/>{{ quarter }}</span></span><br/>
+						  		<strong class="highlight">Prerequisites: </strong><span v-for="prerequisite in obj.prereq"><span></br/>{{ prerequisite }}</span></span>
+						  </div>
+						  <div class="modal-footer">
+								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						  </div>
+					</div>
+		  		</div>
+			  </div> <!--Modal ends-->
+    	</draggable>
 	</div>
-=======
-      <draggable v-model="list" @start="drag=true" @end="drag=false" :options="{group: getPanelId}" class="bucket_area">
-		    <div v-for="course in list" :key="course.code" class="course_box"><strong>{{course.code}}</strong> - {{course.name}}
-	  	  </div>
-      </draggable>
-	  </div>
-  </div>
->>>>>>> ea25f5af739caa69f6859d37d5a59a998600964e
+	</div>
 </template>
 
 <script>
